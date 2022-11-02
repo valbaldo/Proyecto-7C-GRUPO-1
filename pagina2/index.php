e153d6d87f6528f2d3b91112dd4a8996bca7b752
Aguspared <73441981+Aguspared@users.noreply.github.com>
2022-11-02 16:00:30 -0300

    soy un pelotudo

--- pagina2/index.php
+++ pagina2/index.php
@@ -1,6 +1,6 @@
 <?php
     include("assets/php/cn.php");
-    $usuarios="SELECT * FROM perdidos order by id_perdido WHERE encontrado = 0 desc LIMIT 5";
+    $usuarios="SELECT * FROM perdidos WHERE encontrado = 0 order by id_perdido desc LIMIT 5";
     $resultado=mysqli_query($conexion, $usuarios);
 ?>
 
